b2173579e21f5b1a3b072200c7d3b4c9f5ea6439
Regan Bell <regan@Regans-MacBook-Pro.local>
2018-05-05 01:14:40 -0400

    collect update

--- collect_data.m
+++ collect_data.m
@@ -1,26 +1,26 @@
 N = 31;  % number of knot points
 T = 2;   % max duration allowed
-K = 100; % number of iterations
+K = 900; % number of iterations
 [p, traj_opt] = dircol_setup(N, T);
 
 x_traj = [];
 u_traj = [];
 elapsed = zeros(1, K);
-for k = 1:100
+for k = 1:K
     tic;
     x0 = [pi*rand(); 10*rand()]; % random start point
     xf = [pi*rand(); 10*rand()]; % random goal point
     [x_traj, u_traj, x0, pairs, dists] = rand_dircol(p, traj_opt, N, T, x0, xf, x_traj, u_traj);
-    if exist('dircol_data.mat', 'file') ~= 2
+    if exist('dircol_data_3.mat', 'file') ~= 2
         X = pairs;
         Y = dists;
-        save('dircol_data.mat', 'X', 'Y');
+        save('dircol_data_3.mat', 'X', 'Y');
     end
-    load('dircol_data.mat')
+    load('dircol_data_3.mat')
     X = [X pairs];
     Y = [Y dists];
     elapsed(k) = toc;
-    save('dircol_data.mat', 'X', 'Y', 'elapsed');
+    save('dircol_data_3.mat', 'X', 'Y', 'elapsed');
     disp(['Trial: ', num2str(k), '/', num2str(K), ' Total time: ', num2str(sum(elapsed))]);
 end
 
